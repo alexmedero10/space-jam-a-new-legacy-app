925cc269083caa468b1a5f398bc9ebcf832a7bb3
alexmedero10 <alexmedero10@gmail.com>
2021-07-13 19:12:34 -0500

    Finalizada

--- main.js
+++ main.js
@@ -57,6 +57,7 @@ function processPlayers(allPlayerStats) {
 
 // Function to add the players to the bench to start the game
 function displayPlayerBench() {
+
     // Get the bench div in which the players will be shown.
     var bench = document.getElementById('playersOnBench');
 
@@ -64,19 +65,19 @@ function displayPlayerBench() {
     for (let playerName of playerMap.keys()) {
         // Create a button for each player
         var newPlayer = document.createElement('button');
-
+        
         // Set the ID to the name of the player so we can get it later
         newPlayer.id = playerName;
-
+        
         // Identify the style class, which will set the color scheme
         newPlayer.className = 'playerButton';
 
         // When the button is clicked, call the movePlayer function
-        newPlayer.onclick = movePlayer;
-
+        newPlayer.onclick = movePlayers;
+   
         // Add the players image to the button
         var playerImage = document.createElement('img');
-
+        
         // Set the source (or location) of the image
         playerImage.src = 'images/'+playerName+'.png';
 
@@ -85,8 +86,9 @@ function displayPlayerBench() {
 
         // Add the button to the bench
         bench.appendChild(newPlayer);
-    }
 
+    }
+  
     // Display cards for all players
     displayPlayerCards();
 }
